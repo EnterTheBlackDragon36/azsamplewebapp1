425f09e60eec3ab7ebc9c9df67a91877bc70d480
Easton F. Morgan III <41316903+EnterTheBlackDragon36@users.noreply.github.com>
2024-06-14 13:08:15 -0500

    Testing Single Sign On to secure page in Azure

---
+++ Pages/Secured/Secure.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azsamplewebapp1.Pages.Secured
+{
+    public class SecureModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
